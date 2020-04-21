cf717a33f359946dddc54c332c74e61c71c4c1bf
hayouniay <hayouniaymen@yahoo.fr>
2020-04-21 18:29:29 +0200

    commit first

--- calcul.sh
+++ calcul.sh
@@ -3,13 +3,30 @@
 ######################################
 
 #!/bin/bash
-set -x
+
+# For logging level
+# Reset
+Reset='\033[0m'       # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+White='\033[0;37m'        # White
+
+MaxArguments=3
+RightOp="$1";
+Op="$2";
+leftOp="$3";
 
 verif_parameters(){
-  echo $#;
-  if ! [ $# -eq 3 ]; then
-      echo -n "[CALCUL]: Must verify parameters";
+  if ! [ $# != "$MaxArguments" ]; then
+      echo "[CALCUL]: ${Red}ERROR${Reset} - Must verify parameters";
+  fi
+  if [ $op != *[/_*+]* ]; then
+    echo "[CALCUL]: ${Red}ERROR${Reset} - Operation must be in (+ - * /)";
   fi
 }
 
-verif_parameters $1 $2 $3 ;
+verif_parameters $RightOp $Op $leftOp;
